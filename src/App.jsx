3c5ccac0dbea98baf6ae97d1cdc4b114dbc8ebc6
Thomas Wiik <thomas.wiik@no.experis.com>
2024-09-25 15:12:08 +0200

    Completed core and extension 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import Header from './components/Header'
 import initialEmails from './data/emails'
+import { useState } from 'react'
 
 import './styles/App.css'
 
 function App() {
-  // Use initialEmails for state
-  console.log(initialEmails)
+const [emails, setEmails] = useState(initialEmails)
+
+  const toggleRead = (id) => {
+    const updatedEmails = emails.map((email) => 
+      email.id === id ? { ...email, read: !email.read } : email
+    );
+    setEmails(updatedEmails);
+  }
+
+  const toggleStarred = (id => {
+    const updatedEmails = emails.map((email) => 
+      email.id === id ? { ...email, starred: !email.starred } : email
+    );
+    setEmails(updatedEmails);
+  })
 
   return (
     <div className="app">
@@ -19,6 +33,7 @@ function App() {
             <span className="label">Inbox</span>
             <span className="count">?</span>
           </li>
+          
           <li
             className="item"
             // onClick={() => {}}
@@ -28,7 +43,7 @@ function App() {
           </li>
 
           <li className="item toggle">
-            <label for="hide-read">Hide read</label>
+            <label htmlFor="hide-read">Hide read</label>
             <input
               id="hide-read"
               type="checkbox"
@@ -36,11 +51,49 @@ function App() {
               // onChange={() => {}}
             />
           </li>
+          
         </ul>
       </nav>
-      <main className="emails">{/* Render a list of emails here */}</main>
+      <main className="emails">
+        <ul>
+          {emails.map((email) => (
+            <EmailItem
+              key={email.id}
+              email={email}
+              toggleRead={() => toggleRead(email.id)}
+              toggleStarred={() => toggleStarred(email.id)}
+            />
+          ))}
+        </ul>
+      </main>
     </div>
   )
 }
 
-export default App
+function EmailItem({ email, toggleRead, toggleStarred }) {
+  return ( 
+  <li className={`email ${email.read ? 'read' : 'unread'}`}>
+    <div className="select">
+        <input
+          className="select-checkbox"
+          type="checkbox"
+          onClick={toggleRead}/>
+    </div>
+
+    <div className="star">
+        <input
+        className="star-checkbox"
+        type="checkbox"
+        onClick={toggleStarred}
+      />
+    </div>
+    <span className="sender">{email.sender}</span>
+    <span className="title">{email.title}</span>
+  </li>
+  );
+}
+
+
+
+
+export default App;
